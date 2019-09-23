fe49700fa6aaaed6f0ba3570de339a7ebc64f3a6
THRILLER <thrillerlemon@gmail.com>
2019-09-23 10:48:09 +0800

    add cross valind 5 times

---
+++ MTE_JQL_2019_NEW/CrossValind_5/CrossValind5.m
@@ -0,0 +1,46 @@
+% Cross Valind 5 times
+% Linux
+% 2019.9.21
+% JiQiulei thrillerlemon@outlook.com
+clear;clc
+
+
+%第几次交叉验证 i取1~5
+i=5;
+
+
+%Cross Valind data输入path
+path = '/home/JiQiulei/MTE_JQL_2019/';
+out_path = '/home/JiQiulei/MTE_JQL_2019/MTE_RunRes/';
+%读入test数据
+TestSplitX=xlsread([path,'MTE_CorssValind',num2str(i),'.xlsx'], 'SplitX');
+TestRegressX=xlsread([path,'MTE_CorssValind',num2str(i),'.xlsx'], 'RegressX');
+TestY=xlsread([path,'MTE_CorssValind',num2str(i),'.xlsx'], 'Y');
+%读入训练数据
+TrainSplitX=[];
+TrainRegressX=[];
+TrainY=[];
+for n =1:5
+    if n == i
+        continue;
+    end
+    TrainSplitX=[TrainSplitX;xlsread([path,'MTE_CorssValind',num2str(n),'.xlsx'], 'SplitX')];
+    TrainRegressX=[TrainRegressX;xlsread([path,'MTE_CorssValind',num2str(n),'.xlsx'], 'RegressX')];
+    TrainY=[TrainY;xlsread([path,'MTE_CorssValind',num2str(n),'.xlsx'], 'Y')];
+end
+
+%一共有多少条数据
+Allnumber = size(AllSplitX, 1);
+AllSplitX=[TestSplitX;TrainSplitX];
+AllRegressX=[TestRegressX;TrainRegressX];
+AllY=[TestY;TrainY];
+
+%46个变量都是连续变量
+binCat = zeros(1,46);
+
+save([out_path, 'CorssValindVar_',num2str(i)]);
+disp(['save CorssValindVar_',num2str(i)]);
+%建立模型树
+mtbuild(TrainSplitX, TrainRegressX, TrainY, AllSplitX, AllRegressX, AllY, binCat, out_path, ['CorssValind',num2str(i)]);
+
+disp(['Have Done MTE_CorssValind',num2str(i)]);
--- Test.m
+++ Test.m
@@ -1,3 +1,25 @@
 close all;clear;clc
-VARm = double(imread('E:\OFFICE\MTE_NEE_DATA\NHx_N_Deposition1982_2011\NHx_N_Deposition_1988.tif'));
-imshow(VARm)
+%第几次交叉验证 i取1~5
+i=1;
+
+
+%Cross Valind data输入path
+path = 'E:\OFFICE\MTE_NEE_DATA\';
+% out_path = '/home/JiQiulei/MTE_JQL_2019/MTE_RunRes/';
+
+TestSplitX=xlsread([path,'MTE_CorssValind',num2str(i),'.xlsx'], 'SplitX');
+TestRegressX=xlsread([path,'MTE_CorssValind',num2str(i),'.xlsx'], 'RegressX');
+TestY=xlsread([path,'MTE_CorssValind',num2str(i),'.xlsx'], 'Y');
+
+TrainSplitX=[];
+TrainRegressX=[];
+TrainY=[];
+for n =1:5
+    if n == i
+        continue;
+    end
+    TrainSplitX=[TrainSplitX;xlsread([path,'MTE_CorssValind',num2str(n),'.xlsx'], 'SplitX')];
+    TrainRegressX=[TrainRegressX;xlsread([path,'MTE_CorssValind',num2str(n),'.xlsx'], 'RegressX')];
+    TrainY=[TrainY;xlsread([path,'MTE_CorssValind',num2str(n),'.xlsx'], 'Y')];
+end
+disp('aaa')
